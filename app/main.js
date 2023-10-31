f03402463d036bb025290386b2584fb38820e127
Stefan Nguyen <stnguyenvn@gmail.com>
2023-10-30 22:50:54 -0700

    Echo user-agent

--- app/main.js
+++ app/main.js
@@ -1,5 +1,14 @@
 const net = require("net");
 
+function generateResponse(statusCode, statusDescription, headers, body) {
+  const statusLine = `HTTP/1.1 ${statusCode} ${statusDescription}`;
+  const headerLines = Object.entries(headers || []).map(([key, value]) => `${key}: ${value}`);
+  const headerBlock = headerLines.join("\r\n");
+  const bodyBlock = body ? `\r\n\r\n${body}` : '';
+  const response = `${statusLine}\r\n${headerBlock}${bodyBlock}\r\n`;
+  return response;
+}
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   console.log("socket: established");
@@ -12,18 +21,35 @@ const server = net.createServer((socket) => {
     // Parse method and path
     const [method, path] = startLine.split(' ');
 
+    // Parse headers
+    const headers = {};
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i];
+      if (line === '') {
+        break;
+      }
+
+      const [key, value] = line.split(': ');
+      headers[key.toLowerCase()] = value;
+    }
+
+    // Paths
     if (path === '/') {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.write(generateResponse(200, 'OK'));
     } else if (path.startsWith("/echo/")) {
       const msg = path.slice(6);
       const msgLength = Buffer.byteLength(msg, "utf8");
-      socket.write(`HTTP/1.1 200 OK
-Content-Type: text/plain
-Content-Length: ${msgLength}
-
-${msg}
-      `.replace(/\n/g, "\r\n"));
-
+      socket.write(generateResponse(200, 'OK', {
+        'Content-Type': 'text/plain',
+        'Content-Length': msgLength,
+      }, msg));
+    } else if (path === '/user-agent') {
+      const msg = headers['user-agent'];
+      const msgLength = Buffer.byteLength(msg, "utf8");
+      socket.write(generateResponse(200, 'OK', {
+        'Content-Type': 'text/plain',
+        'Content-Length': msgLength,
+      }, msg));
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
