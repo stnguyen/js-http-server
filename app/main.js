63a8107d4f73dc86417008fbb2a9485d9346198b
Stefan Nguyen <stnguyenvn@gmail.com>
2023-10-30 22:35:57 -0700

    Echo

--- app/main.js
+++ app/main.js
@@ -14,6 +14,16 @@ const server = net.createServer((socket) => {
 
     if (path === '/') {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.startsWith("/echo/")) {
+      const msg = path.slice(6);
+      const msgLength = Buffer.byteLength(msg, "utf8");
+      socket.write(`HTTP/1.1 200 OK
+Content-Type: text/plain
+Content-Length: ${msgLength}
+
+${msg}
+      `.replace(/\n/g, "\r\n"));
+
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
