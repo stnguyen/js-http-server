087ea5824ada9330f4a91694eca562b464a5264b
Stefan Nguyen <stnguyenvn@gmail.com>
2023-10-30 22:17:09 -0700

    Reply with http 200

--- app/main.js
+++ app/main.js
@@ -5,6 +5,8 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  console.log("connection established");
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
     server.close();
