773c6ce6024ab25374b51d7bbb842c4490b4b25c
Stefan Nguyen <stnguyenvn@gmail.com>
2023-10-30 22:23:41 -0700

    Send on data

--- app/main.js
+++ app/main.js
@@ -1,18 +1,17 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  console.log("connection established");
+  console.log("socket: established");
+
   socket.on('data' , (data) => {
     console.log('data:', data.toString());
-  });
-  socket.on('end' , () => {
     socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.end();
   });
+
   socket.on("close", () => {
+    console.log("socket: on('close')");
     socket.end();
     server.close();
   });
